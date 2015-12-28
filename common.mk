1bab12e416d5267ddfce46f76485d4500a7d4162
Oskar Pearson <oskar@deckle.co.uk>
2015-12-28 01:55:41 +0000

    Parameterise makefile
    Move almost everything into a central common.mk file

--- common.mk
+++ common.mk
@@ -1,18 +1,36 @@
 
 CC=sdcc
+LDFLAGS=--xram-loc 0xf000 --xram-size 0x1000
 
-default: output output/subg_rfspy.hex
+default: output output/${TARGET_BUILD} output/${TARGET_BUILD}/${TARGET_BUILD}.hex
+
+common_modules = radio.rel main.rel timer.rel \
+	           commands.rel delay.rel
+
+clean:
+	rm -rf output/${TARGET_BUILD}
+
+%.rel: %.c
+	$(CC) $(CFLAGS) -o output/${TARGET_BUILD}/$@ -c $<
+
+# The serial implementation used depends on the TARGET_BUILD parameter, so is built
+# separately from the other .rel files
+serial.rel: ${TARGET_BUILD}/serial.c
+	$(CC) $(CFLAGS) -o output/${TARGET_BUILD}/$@ -c $<
+
+output/${TARGET_BUILD}/${TARGET_BUILD}.hex: $(common_modules) serial.rel
+	cd output/${TARGET_BUILD} && $(CC) $(LDFLAGS) $(CFLAGS) $(common_modules) serial.rel -o ${TARGET_BUILD}.hex
+
+install: output/${TARGET_BUILD}/${TARGET_BUILD}.hex
+	cc-tool -n cc1110 --log install.log -ew output/${TARGET_BUILD}/${TARGET_BUILD}.hex
+
+test: main.c output
+	gcc -g -o output/${TARGET_BUILD}/test -DNON_NATIVE_TEST main.c
 
 output:
 	mkdir output
 
-output/%.rel : %.c Makefile
+output/${TARGET_BUILD}:
+	mkdir output/${TARGET_BUILD}/
 
-%.rel: %.c
-	$(CC) $(CFLAGS) -o output/$@ -c $<
-
-common_modules = radio.rel main.rel timer.rel \
-           commands.rel delay.rel
-
-clean:
-	rm -rf output 
+output/${TARGET_BUILD}/%.rel : %.c Makefile
