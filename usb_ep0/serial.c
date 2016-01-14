1099e24fa16e3417c15117a9486f78a078a05a19
Ben West <bewest@gmail.com>
2016-01-14 15:47:15 -0800

    quickly stub out usb side
    heavily influenced by CC-Bootloader.

---
+++ usb_ep0/serial.c
@@ -0,0 +1,38 @@
+
+#include <stdint.h>
+#include "hardware.h"
+#include "serial.h"
+#include "radio.h"
+#include "hal.h"
+#include "usb.h"
+
+void configure_serial()
+{
+
+  usb_init( );
+  usb_up( );
+
+}
+
+uint8_t serial_rx_byte() {
+  // return (uint8_t) usb_getc( );
+  return (uint8_t) usb_getchar( );
+} 
+
+uint16_t serial_rx_word() {
+  return (serial_rx_byte() << 8) + serial_rx_byte();
+}
+
+void serial_tx_byte(uint8_t tx_byte) {
+  // return usb_putc(tx_byte);
+  return usb_putchar((char) tx_byte);
+}
+
+void serial_tx_str(const char *str) {
+  while(*str != 0) {
+    serial_tx_byte(*str);
+    str++;
+  }
+  serial_tx_byte(0);
+}
+
---
+++ usb_ep0/serial.h
@@ -0,0 +1,13 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+const int SERIAL_DATA_AVAILABLE = 0;
+
+void configure_serial();
+void serial_tx_byte(uint8_t);
+void serial_tx_str(const char *str);
+void run_command_from_serial();
+uint8_t serial_rx_byte();
+uint16_t serial_rx_word();
+
+#endif
