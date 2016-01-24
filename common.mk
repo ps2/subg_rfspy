73beebef15b3ddff752a45dca9f4b718d9ce38bd
Ben West <bewest@gmail.com>
2016-01-23 19:56:38 -0800

    make -f Makefile.usb_ep0 BOOTLOADER=1

--- common.mk
+++ common.mk
@@ -10,6 +10,10 @@ CC=sdcc
 LDFLAGS=--xram-loc 0xf000 --xram-size 0x1000
 CFLAGS=-I. -I${SERIAL_TYPE} --verbose ${RADIO_LOCALE_DEF} -D${BOARD_TYPE} ${BOARD_PARAMS} ${SERIAL_PARAMS}
 
+BOOTLOADER_FLAGS = 
+ifdef BOOTLOADER
+BOOTLOADER_FLAGS = --code-loc 0x1400
+endif
 default: output output/${TARGET_BUILD} output/${TARGET_BUILD}/${TARGET_BUILD}.hex
 
 common_modules = radio.rel main.rel timer.rel \
@@ -28,7 +32,7 @@ serial.rel: ${SERIAL_TYPE}/serial.c
 	$(CC) $(CFLAGS) -o output/${TARGET_BUILD}/$@ -c $< $(REL)
 
 output/${TARGET_BUILD}/${TARGET_BUILD}.hex: $(common_modules) $(REL) serial.rel
-	cd output/${TARGET_BUILD} && $(CC) $(LDFLAGS) $(CFLAGS) $(common_modules) $(REL) serial.rel  -o ${TARGET_BUILD}.hex
+	cd output/${TARGET_BUILD} && $(CC) $(LDFLAGS) $(BOOTLOADER_FLAGS) $(CFLAGS) $(common_modules) $(REL) serial.rel  -o ${TARGET_BUILD}.hex
 
 install: output/${TARGET_BUILD} output/${TARGET_BUILD}/${TARGET_BUILD}.hex
 	sudo cc-tool -n cc1110 --log install.log -ew output/${TARGET_BUILD}/${TARGET_BUILD}.hex
